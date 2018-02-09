34813ba5a3278e36acfa9273e068e7269fb7ddf1
John Yingling <hotmetaldobermans@gmail.com>
2018-02-09 11:46:24 +0800

    script change...
    bleep!

--- HW-2/script.js
+++ HW-2/script.js
@@ -1,1 +1,12 @@
+<title>Ni Hao World!  Bleep!  </title>
 
+<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript">
+
+</script>
+
+
+<script>
+		console.log("Hello World!  May all have mercy on my soul!");
+
+</script>
